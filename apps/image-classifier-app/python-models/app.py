855fe4a0f5199273c3bd3b8f75c865f0279b9e49
enggabhishek <abhiemin@gmail.com>
2024-12-05 19:11:16 -0500

    update

--- apps/image-classifier-app/python-models/app.py
+++ apps/image-classifier-app/python-models/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 import os
-# from tf_models import predictImagesClass
+from tf_models import predictImagesClass
 # from pytorch_models import predict_images_class
-# import io
-# from dotenv import load_dotenv
-# load_dotenv()
+import io
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all domains
@@ -23,25 +23,30 @@ def classify_image():
     # image_data = request.files["image"]
     # image_bytes = io.BytesIO(image_data.read())
     # image_data.seek(0)
+    image_bytes='/var/www/web-app/data/Measles/1.jpg'
     model_name = request.form['model']
     print("It is working",model_name)
     
-    # if model_name == 'resnet50':
-    #     result = predict_images_class(model_name, image_bytes)
+    if model_name == 'resnet50':
+        # result = predict_images_class(model_name, image_bytes)
+        pass
         
-    # elif model_name == 'nesnet_a_large':
-    #     result = predict_images_class(model_name, image_bytes)
+    elif model_name == 'nesnet_a_large':
+        # result = predict_images_class(model_name, image_bytes)
+        pass
         
-    # elif model_name == 'inception_resnet_v2':
-    #     model_name=os.getenv('MODEL_LOC')+'inception_resnet_v2.keras'
-    #     print(model_name)
-    #     result=predictImagesClass(model_name, image_bytes, image_width=299, image_height=299)
-    #     print(result)
+    elif model_name == 'inception_resnet_v2':
+        model_name=os.getenv('MODEL_LOC')+'inception_resnet_v2.keras'
+        print(model_name)
+        result=predictImagesClass(model_name, image_bytes, image_width=299, image_height=299)
+        pass
+        print(result)
         
-    # elif model_name == 'custom_cnn':
-    #     model_name=os.getenv('MODEL_LOC')+'custom_cnn.keras'
-    #     print(model_name)
-    #     result=predictImagesClass(model_name, image_bytes, image_width=256, image_height=256)
+    elif model_name == 'custom_cnn':
+        model_name=os.getenv('MODEL_LOC')+'custom_cnn.keras'
+        print(model_name)
+        result=predictImagesClass(model_name, image_bytes, image_width=256, image_height=256)
+        pass
 
     response = {
         "status": "success",
